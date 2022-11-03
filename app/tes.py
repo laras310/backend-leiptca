90a73395159b9f145aae9b91a9c78eb7b4851e90
Laras310 <nurullarasati310@gmail.com>
2022-11-03 23:35:28 +0700

    load dictionary

--- app/tes.py
+++ app/tes.py
@@ -15,54 +15,26 @@ cursor = mydb.cursor(dictionary=True)
 app.secret_key="abcd"
 
 @app.route('/login', methods=['POST'])
-# def login():
-#      #Pengambilan data dari Form Login.html (email, Password)
-#     if not 'loggedin' in session:
-#         if request.method == 'POST' and 'email' in request.json and 'password' in request.json:
-#             email = request.json['email']
-#             password = request.json['password']
-
-#             cursor.execute('SELECT * FROM user where email = %s AND password = %s', (email, password))
-#             user = cursor.fetchone()
-           
-#             # jika user ditemukan
-#             if user:
-#                 session['loggedin'] = True
-#                 session['username'] = user['username']
-#                 session['role'] = user['role']
-
-#                 return jsonify({"msg":"login sukses"})
-#             else:
-#               return jsonify({"msg":"pw salah"})
-#         else:
-#           return jsonify({"msg":"blm masukin pw"})
-#     else:
-#       return jsonify({"msg":"blm masukin email"})
-
 def login():
-    msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        # Check if account exists using MySQL
-        cursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-        # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
-            return 'Logged in successfully!'
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    # Show the login form with message (if any)
-    return jsonify(msg)
+  if not 'loggedin' in session:
+    # if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+      email = request.json['email']
+      password = request.json['password']
+
+      cursor.execute('SELECT * FROM user where email = %s AND password = %s', (email, password))
+      user = cursor.fetchone()
+      
+      # jika user ditemukan
+      if user:
+          session['loggedin'] = True
+          session['username'] = user['username']
+          session['role'] = user['role']
+
+          return jsonify({"msg":"login sukses"})
+      else:
+        return jsonify({"msg":"pw salah"})
+    # return jsonify({"msg":"pw salah"})
+  return jsonify({"msg":"render template login"})
 
 @app.route('/logout', methods=['POST'])
 def logout():
@@ -73,6 +45,7 @@ def logout():
 
 @app.route('/signup', methods=['POST'])
 def signup():
+  name = request.json["name"]
   email = request.json["email"]
   password = request.json["password"]
 
@@ -83,13 +56,14 @@ def signup():
   if user_exists:
     abort(409)
   
-  new_user=user(email=email, password=password, role='client')
+  new_user=user(name = name, email=email, password=password, role='client')
   mydb.session.add(new_user)
   mydb.session.commit()
 
   return jsonify({
     "id": new_user.id,
-    "email": new_user.email
+    "email": new_user.email,
+    "name":new_user.name
   })
 
 @app.route('/article', methods=['GET'])
@@ -99,13 +73,18 @@ def article():
 
   return jsonify(articles)
 
-@app.route('/cek', methods=['GET'])
-def cek():
-  cursor.execute('SELECT * FROM user')
-  user = cursor.fetchall()
-  return {
-    "username" : user[0]
-  }
+@app.route('/dictionary/<language>/<alphabet>', methods=['GET'])
+def dictionary(language, alphabet):
+  if language == "english":
+    cursor.execute('SELECT * FROM dict_eng where word LIKE "{}%"'.format(alphabet))
+  
+  elif language == "indonesia":
+    cursor.execute('SELECT * FROM dict_ind where word LIKE "{}%"'.format(alphabet))
+  
+  else :
+    return jsonify({"msg":"gada bahasanya"})
+  data = cursor.fetchall()
+  return jsonify(data)
 
 
 if __name__ == 'main':
