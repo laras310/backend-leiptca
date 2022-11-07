f5901034c050c7e5acce7a9c64e30b89e8b90964
Laras310 <nurullarasati310@gmail.com>
2022-11-07 21:06:00 +0700

    add requirement.txt amd app.yaml

--- app/tes.py
+++ app/tes.py
@@ -1,6 +1,8 @@
 from urllib import response
 import mysql.connector
 from flask import Flask, make_response, render_template, request, redirect, url_for, session, send_file, abort, jsonify
+from flask_cors import CORS
+from datetime import date
 
 mydb = mysql.connector.connect(
   host="127.0.0.1",
@@ -11,6 +13,7 @@ mydb = mysql.connector.connect(
 
 # inisiasi variabel aplikasi
 app = Flask(__name__)
+CORS(app)
 cursor = mydb.cursor(dictionary=True)
 app.secret_key="abcd"
 
@@ -94,9 +97,7 @@ def dictionary(language, alphabet):
   else :
     return jsonify({"msg":"gada bahasanya"})
   data = cursor.fetchall()
-  data2= jsonify(data)
-  data2.headers.add('Access-Control-Allow-Origin', '*')
-  return data2
+  return jsonify(data)
 
 @app.route('/user/<id>', methods=['GET'])
 def user(id):
@@ -190,5 +191,90 @@ def add_admin():
       return jsonify(new_user)
   return jsonify({"msg":"salah method/gada form nama/email/pq"})
 
+@app.route('/roles', methods=['GET'])
+def option():
+  cursor.execute('SELECT * FROM roles')
+  roles = cursor.fetchall()
+  return jsonify(roles)
+
+@app.route('/orders/<id>', methods=['GET'])
+def orders(id):
+  if not id=="all":
+    cursor.execute('SELECT * FROM ordered where order_id = %s',([id]))
+    order = cursor.fetchone()
+    return jsonify(order)
+  else:
+    cursor.execute('SELECT * FROM ordered ORDER BY order_date DESC')
+    orders = cursor.fetchall()
+    return jsonify(orders)
+
+@app.route('/progress/<id>', methods=['GET'])
+def progress(id):
+  cursor.execute('SELECT * FROM progress WHERE order_id = %s ORDER BY progress_num ASC',([id]))
+  progress = cursor.fetchall()
+  return jsonify(progress)
+
+@app.route('/services_list/<service_type>', methods=['GET'])
+def services_list(service_type):
+  if service_type == "legal":
+    cursor.execute('SELECT * FROM legal_list ORDER BY type ASC')
+  elif service_type == "translate":
+    cursor.execute('SELECT * FROM translate_list')
+  elif service_type == "training":
+    cursor.execute('SELECT * FROM training_list ORDER BY date DESC')
+  services = cursor.fetchall()
+  return jsonify(services)
+
+
+# @app.route('/add_progress/<id>', methods=['POST'])
+# def add_progress(id):
+#   if request.method == 'POST' and 'status' in request.json and 'desc' in request.json:
+#     status = request.json["status"]
+#     email = request.json["email"]
+#     password = request.json["password"]
+#     role = request.json["role"]
+#     phone_number = request.json["phone_number"]
+
+#     cursor.execute('SELECT status FROM user where email = %s',([email]))
+#     user = cursor.fetchone()
+  
+#     if user:
+#       return jsonify({"msg":"usernya udah ada"})
+    
+#     else:
+#       cursor.execute('INSERT INTO user (email, name, password, role) VALUES (%s,%s,%s,%s)', (email, name, password, role))
+#       mydb.commit()
+#       cursor.execute('SELECT user_id FROM user where email = %s',([email]))
+#       user_id = cursor.fetchone()
+#       print(user_id)
+#       cursor.execute('INSERT INTO additional_info (phone_number, user_id) VALUES (%s,%s)', (phone_number,user_id[0]))
+#       mydb.commit()
+#       cursor.execute('SELECT * FROM user where email = %s',([email]))
+#       new_user = cursor.fetchone()
+#       return jsonify(new_user)
+#   return jsonify({"msg":"salah method/gada form nama/email/pq"})
+
+# @app.route('/legal_order', methods=['POST'])
+# def legal_order():
+#   if request.method == 'POST' and 'legal_service' in request.json and 'delivery' in request.json and 'voucher' in request.json:
+#     legal_service_id = request.json["legal_service_id"]
+#     delivery = request.json["delivery"]
+#     voucher = request.json["voucher"]
+#     date = date.today()
+
+#     cursor.execute('SELECT user_id FROM user WHERE ')
+  
+#     if user:
+#       return jsonify({"msg":"usernya udah ada"})
+    
+#     else:
+#       cursor.execute('INSERT INTO ordered (order_id, order_service_id, user_id, order_date, order_cost, order_desc) VALUES (%s,%s,%s,%s)', (order_id, legal_service_id, user_id, date, cost, desc))
+#       mydb.commit()
+#       cursor.execute('SELECT * FROM user where email = %s',([email]))
+#       new_user=cursor.fetchone()
+#       return jsonify(new_user)
+#   return jsonify({"msg":"salah method/gada form nama/email/pq"})
+
+
 if __name__ == 'main':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=8081, debug=True)
