2f1792e60394416bbbc0fce6981845abcfc189ce
Laras310 <nurullarasati310@gmail.com>
2022-11-12 22:12:13 +0700

    update db

--- backup_db/leiptca_web.sql
+++ backup_db/leiptca_web.sql
@@ -269,7 +269,7 @@ DROP TABLE IF EXISTS `notification`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `notification` (
-  `notif_id` int(11) NOT NULL,
+  `notif_id` int(11) NOT NULL AUTO_INCREMENT,
   `status` tinyint(1) NOT NULL,
   `type` varchar(50) NOT NULL,
   `title` varchar(50) NOT NULL,
@@ -489,4 +489,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-12 21:52:07
+-- Dump completed on 2022-11-12 22:11:45
