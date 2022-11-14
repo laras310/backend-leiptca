6e210f9b0df74dd76a5e79dd87a58c344918a534
Laras310 <nurullarasati310@gmail.com>
2022-11-14 22:41:56 +0700

    .

--- app/base.py
+++ app/base.py
@@ -8,7 +8,25 @@ from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
 api = Flask(__name__)
 
 api.config["JWT_SECRET_KEY"] = "please-remember-to-change-me"
+api.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(api)
+
+@api.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
 
 @api.route('/token', methods=["POST"])
 def create_token():
@@ -21,7 +39,14 @@ def create_token():
     response = {"access_token":access_token}
     return response
 
+@api.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
 @api.route('/profile')
+@jwt_required()
 def my_profile():
     response_body = {
         "name": "Nagato",
